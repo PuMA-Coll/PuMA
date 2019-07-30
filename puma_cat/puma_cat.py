b2aa95846ab647db0cacf99977758022a53e21ee
egutierrezposse <egutierrezposse@gmail.com>
2019-07-30 18:23:42 -0300

    Hay que revisar que la formula para el minimo flujo detectable este bien, que las unidades sean las correctas, y empezar a agregarle en el ultimo for lo que queremos que cree

---
+++ puma_cat/puma_cat.py
@@ -0,0 +1,51 @@
+# Must install psrqpy first: https://github.com/mattpitkin/psrqpy
+from psrqpy import *
+from configparser import ConfigParser
+from astropy import units as u
+from astropy.coordinates import Angle
+
+SNR_min = 10.0
+
+#########################################################################################################
+class Antenna:     
+    def __init__(self,a_param_location,name):
+        a_param = ConfigParser()
+        a_param.read(a_param_location)
+        
+        self._bandwidth = a_param.getfloat(name,'bandwidth')
+        self._nu_central = a_param.getfloat(name,'nu_central')
+        self._gain = a_param.getfloat(name,'gain')
+        self._system_temp = a_param.getfloat(name,'t_sys')
+        self._npol = a_param.getfloat(name,'n_pol')
+        self._beta_g = a_param.getfloat(name,'beta_g')
+        self._obs_time = a_param.getfloat(name,'t_obs')*60.0
+        self._DECJlimit = a_param.getfloat(name,'maxDECJ') 
+        
+    def can_detect(self,psr):
+        psr_S1400_Jy = psr.S1400/1000.0
+        psr_W50_sec = psr.W50/1000.0
+        psr_flux = psr_S1400_Jy * np.sqrt((psr.P0-psr_W50_sec)/psr_W50_sec)
+        min_flux = SNR_min * self._system_temp / np.sqrt(self._npol*self._obs_time*self._bandwidth)
+        cond1 = (psr_flux > min_flux)
+        psr_DECJ = Angle(psr.DECJ,unit=u.deg).to_value()
+        cond2 = (psr_DECJ < self._DECJlimit)
+        return (cond1 and cond2)
+#####################################################################################################
+
+def IAR_can_detect(pulsar):
+    return (A1.can_detect(pulsar) or A2.can_detect(pulsar))
+
+#####################################################################################################
+
+antennas_file = './antenna_parameters.dat'
+A1 = Antenna(antennas_file,'A1')
+A2 = Antenna(antennas_file,'A2')
+
+query = QueryATNF(condition='S1400 > 0 && W50 > 0')
+psrs = query.get_pulsars()
+
+for psr in psrs:
+    if(IAR_can_detect(psrs[psr])):
+        f = open(psr+'.par','w')
+        f.write(query.get_ephemeris(psr))
+        f.close()
