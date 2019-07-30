b66d68343733e4ac6756e1b65d1ae61683cb9fc0
egutierrezposse <egutierrezposse@gmail.com>
2019-07-30 20:32:24 -0300

    crea .par y .sh para pointing para ambas antenas

--- puma_cat/puma_cat.py
+++ puma_cat/puma_cat.py
@@ -12,6 +12,7 @@ class Antenna:
         a_param = ConfigParser()
         a_param.read(a_param_location)
         
+        self._name = name
         self._bandwidth = a_param.getfloat(name,'bandwidth')
         self._nu_central = a_param.getfloat(name,'nu_central')
         self._gain = a_param.getfloat(name,'gain')
@@ -19,7 +20,8 @@ class Antenna:
         self._npol = a_param.getfloat(name,'n_pol')
         self._beta_g = a_param.getfloat(name,'beta_g')
         self._obs_time = a_param.getfloat(name,'t_obs')*60.0
-        self._DECJlimit = a_param.getfloat(name,'maxDECJ') 
+        self._DECJlimit = a_param.getfloat(name,'maxDECJ')
+        self._pointer_file = a_param.get(name,'pointer_file')
         
     def can_detect(self,psr):
         psr_S1400_Jy = psr.S1400/1000.0
@@ -30,6 +32,19 @@ class Antenna:
         psr_DECJ = Angle(psr.DECJ,unit=u.deg).to_value()
         cond2 = (psr_DECJ < self._DECJlimit)
         return (cond1 and cond2)
+    
+    def create_parfile(self,psr):
+        par = open(psr.name+'.par','w')
+        par.write(query.get_ephemeris(psr.name))
+        par.close()
+    
+    def create_pointer(self,psr):
+        pointer = open(psr.name+'_'+self._name+'.sh','w')
+        pointer.write('#!/bin/bash'+'\n')
+        pointer.write('# ./pulsar_usrp.sh RA_ang DEC_ang time_seconds Name'+'\n')
+        pointer.write('./'+self._pointer_file+' '+psr.DECJ+' '+psr.RAJ+' '+psr.name+'\n')
+        pointer.close()
+
 #####################################################################################################
 
 def IAR_can_detect(pulsar):
@@ -41,11 +56,15 @@ antennas_file = './antenna_parameters.dat'
 A1 = Antenna(antennas_file,'A1')
 A2 = Antenna(antennas_file,'A2')
 
+#####################################################################################################
+
 query = QueryATNF(condition='S1400 > 0 && W50 > 0')
 psrs = query.get_pulsars()
 
+#####################################################################################################
+
 for psr in psrs:
     if(IAR_can_detect(psrs[psr])):
-        f = open(psr+'.par','w')
-        f.write(query.get_ephemeris(psr))
-        f.close()
+        A1.create_parfile(psrs[psr])
+        A1.create_pointer(psrs[psr])
+        A2.create_pointer(psrs[psr])
