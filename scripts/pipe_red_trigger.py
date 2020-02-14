4406cd658bdd956aaceb2bf5bcfff524265dac3e
Santiago del Palacio <santiagodp1990@gmail.com>
2020-02-14 09:10:48 -0300

    First version of red alert pipe

--- scripts/pipe_red_trigger.py
+++ scripts/pipe_red_trigger.py
@@ -19,3 +19,57 @@ import parfile
 from puma_reduc import do_reduc
 
 
+
+def set_argparse():
+    # add arguments
+    parser = argparse.ArgumentParser(prog='puma_reduc.py',
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+            description='raw data folding with PRESTO')
+    parser.add_argument('--folder', default=os.environ['PWD'], type=str,
+            help='ABSOLUTE PATH where observations are stored and where output will be created')
+    parser.add_argument('--par_dirname', default='/opt/pulsar/tempo/tzpar/', type=str,
+            help='path to directory containing .par file')
+
+    return parser.parse_args()
+
+
+
+def check_cli_arguments(args):
+
+    ierr = 0
+
+    if os.path.isabs(args.folder) is False:
+        print('\n FATAL ERROR: folder path is not absolute\n')
+        ierr = -1
+        return ierr
+
+    return ierr
+
+#==================================================================================
+
+if __name__ == '__main__':
+
+    # get cli-arguments
+    args = set_argparse()
+
+    # check arguments
+    ierr = check_cli_arguments(args)
+    if ierr != 0:
+        sys.exit(1)
+    else:
+        start = time.time()
+
+    ierr = do_reduc(ftype='timing', folder=args.folder, par_dirname=args.par_dirname, ncores=2)
+    if ierr != 0:
+        sys.exit(1)
+
+    ierr = do_reduc(ftype='par', folder=args.folder, par_dirname=args.par_dirname, ncores=2)
+    if ierr != 0:
+        sys.exit(1)
+
+
+    # exit with success printing duration
+    end = time.time()
+    hours, rem = divmod(end-start, 3600)
+    minutes, seconds = divmod(rem, 60)
+    print('\n Reduction process completed in {:0>2}:{:0>2}:{:05.2f}\n'.format(int(hours), int(minutes), seconds))
