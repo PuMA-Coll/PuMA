c8d531a2fdc1cb5c71da6acb6f6f28cb3b6ae46e
Santiago del Palacio <santiagodp1990@gmail.com>
2020-02-14 15:20:59 -0300

    Forgot to add files in previous commit. Santiago-s fault

---
+++ scripts/pipe_pugliS.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python2
+
+#Author: PuGli-S
+#Date: Feb 2020
+
+import os
+import sys
+sys.path.insert(1,os.path.join(sys.path[0], '/opt/pulsar/puma/scripts/'))
+import time
+import argparse
+
+from ConfigParser import SafeConfigParser
+import glob
+import sigproc
+import subprocess
+import parfile
+
+from puma_reduc import do_reduc
+from pipe_red_trigger import glitch_search
+from puma_lib import Observation
+
+
+
+def set_argparse():
+   # add arguments
+   parser = argparse.ArgumentParser(prog='pipe_pugliS.py',
+      formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+      description='Main pipeline for pulsar timing and glitch detections')
+   parser.add_argument('--folder', default=os.environ['PWD'], type=str,
+      help='ABSOLUTE PATH where observations are stored and where output will be created')
+   parser.add_argument('--par_dirname', default='/opt/pulsar/tempo/tzpar/', type=str,
+      help='path to directory containing .par file')
+   parser.add_argument('--thresh', default=1e-8, type=float,
+      help='threshold for glitch alert (DP/P > thresh)')
+
+   return parser.parse_args()
+
+
+
+def check_cli_arguments(args):
+
+   ierr = 0
+   if os.path.isabs(args.folder) is False:
+      print('\n FATAL ERROR: folder path is not absolute\n')
+      ierr = -1
+      return ierr
+
+   return ierr
+
+
+
+def send_alert(alert_type):
+   if alert_type == 'red':
+      print('\n \x1b[31;1m GLITCH RED ALERT! \x1b[0m \n')
+
+   elif alert_type == 'blue':
+      print('\n \x1b[34;1m glitch blue alert \x1b[0m \n')
+
+
+
+def write_pugliS_info():
+   """ Write information"""
+   pass
+   
+
+#==================================================================================
+
+if __name__ == '__main__':
+
+   # get cli-arguments
+   args = set_argparse()
+
+   # check arguments
+   ierr = check_cli_arguments(args)
+   if ierr != 0: sys.exit(1)
+
+   start = time.time()
+
+   # move observations to destination folder par_dirname
+   # (to complete)
+
+   # read relevant information from the .fil
+   obs = Observation(args.folder)
+
+   # search for glitches (code red)
+   obs.do_glitch_search(thresh)
+   if obs.red_alert: send_red_alert('red')
+
+   # search for glitches (code blue)
+   #obs.do_glitch_search(thresh)
+   # if blue_alert: send_alert('blue')
+
+   # write observation info
+   obs.write_pugliS_info()
+
+   # move files for visualization in ...
+   # (to do)
+
+   # call updater for webpage
+   # (puglieseweb_update)
+
+   # exit with success printing duration
+   end = time.time()
+   hours, rem = divmod(end-start, 3600)
+   minutes, seconds = divmod(rem, 60)
+   print('\n Reduction process completed in {:0>2}:{:0>2}:{:05.2f}\n'.format(int(hours), int(minutes), seconds))
---
+++ scripts/puma_lib.py
@@ -0,0 +1,34 @@
+import sigproc
+import glob
+
+class Observation(object):
+	""" This is the main class that represents an observation from a single antenna."""
+
+	def __init__(self, path2dir=''):
+		self.dir_path = path2dir
+		self.pname, self.antenna, self.mjd, self.nchans = self.get_pulsar_parameters()
+		self.glitch = False
+		self.red_alert = False
+		self.blue_alert = False
+		self.par_dir = '/opt/pulsar/tempo/tzpar/'
+		self.par = '/opt/pulsar/tempo/tzpar/' + self.pname + '.par'
+
+
+	def get_pulsar_parameters():
+	    # select .fil file
+    	fils = glob.glob(self.dir_path + '/*.fil')  	
+        fil = fils[0]
+
+    	# grab name of pulsar from the .fil with sigproc function read_header (dictionary)
+    	fil_dic = sigproc.read_header(fil)[0]
+		pname = fil_dic['source_name'][:-3]
+		antenna = fil_dic['source_name'][-2:]
+		mjd = fil_dic['tstart']
+		nchans = fil_dic['nchans']
+
+		return pname, antenna, mjd, nchans
+
+
+	def do_glitch_search(threshold):
+		self.red_alert, self.jump = glitch_search(folder=self.folder, 
+			par_dirname=self.par_dirname, ncores=2, thresh=threshold)
