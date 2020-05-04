eaf1a0697ec8810cc4fd8a44f6ad8cf8ffdba82e
egutierrezposse <puma@iar.unlp.edu.ar>
2020-05-04 22:32:46 +0000

    Added if statement so it exits if there are no pfds in the folder

--- scripts/puma_template.py
+++ scripts/puma_template.py
@@ -21,22 +21,28 @@ pwd = os.getcwd()
 snr_max = 0.0
 pfd_max = ''
 pfds = [pfd for x in os.walk('.') for pfd in glob.glob(os.path.join(x[0], '*.pfd'))]
-for pfd in pfds:
-    arch = psrchive.Archive_load(pfd)
-    snr_string = subprocess.check_output(['psrstat','-j','pF','-c','snr',arch.get_filename()])
-    snr = float(snr_string[snr_string.index('snr')+4:-1])
-    if snr > snr_max:
-        snr_max = snr
-        pfd_max = pfd
 
-aux_index = pfd_max.index('20')
-print('The best observation is from ' + pfd_max[aux_index:aux_index+8] + ' and has S/N = '+str(snr_max)+'\n')
-arch = psrchive.Archive_load(pfd_max)
+if pfds == []:
+    print('There are no pfds in this folder!')
+    sys.exit(1)
 
-# Make a smooth profile with the best observation
+else:
+    for pfd in pfds:
+        arch = psrchive.Archive_load(pfd)
+        snr_string = subprocess.check_output(['psrstat','-j','pF','-c','snr',arch.get_filename()])
+        snr = float(snr_string[snr_string.index('snr')+4:-1])
+        if snr > snr_max:
+            snr_max = snr
+            pfd_max = pfd
 
-subprocess.check_output(['psrsmooth', '-n', '-e', 'std', pfd_max])
-fname = arch.get_filename()[1:] + '.std'
-new_fname = 'J' + fname.split('_')[-1]
-subprocess.check_output(['mv', os.getcwd()+arch.get_filename()[1:] + '.std', pwd + '/' + new_fname])
+    aux_index = pfd_max.index('20')
+    print('The best observation is from ' + pfd_max[aux_index:aux_index+8] + ' and has S/N = '+str(snr_max)+'\n')
+    arch = psrchive.Archive_load(pfd_max)
+
+    # Make a smooth profile with the best observation
+
+    subprocess.check_output(['psrsmooth', '-n', '-e', 'std', pfd_max])
+    fname = arch.get_filename()[1:] + '.std'
+    new_fname = 'J' + fname.split('_')[-1]
+    subprocess.check_output(['mv', os.getcwd()+arch.get_filename()[1:] + '.std', pwd + '/' + new_fname])
 
