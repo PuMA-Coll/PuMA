2b973ddcbccf474c45dfdd49372f981e1a3dd342
Santiago del Palacio <santiagodp1990@gmail.com>
2020-05-07 20:16:34 -0300

    Massive changes to puma_timing.py. Among these, added argument for output directory where the plot and the residuals file will be stored.

--- scripts/puma_timing.py
+++ scripts/puma_timing.py
@@ -1,103 +1,55 @@
 #!/usr/bin/env python3
 #
 # PuMA timing
-# Python script for obtaining time of arrivals
+# Python script for obtaining and plotting residuals
 # Author: PuMA collab.
 # April 2020
 
-# BORRAR LAS QUE NO SE USEN
 import argparse
-import time
 import numpy as np
 import shutil
-
-import decimal
-import subprocess
 import math
-
 import os
+
 import scipy.stats
-
 import libstempo as T2
+import matplotlib.pyplot as P
 
 
-def set_argparse():
-    # add arguments
-    parser = argparse.ArgumentParser(prog='puma_timing.py',
-            formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-            description='calculate residuals using tempo2')
-    parser.add_argument('--par_fname', default='', type=str,
-            help='path to directory containing .par file')
-    parser.add_argument('--tim_fname', default='', type=str,
-            help='path to .tim file')
+def calc_residuals_errorbars(rms, n_obs):
+    """" Calculate the rms uncertainties at 1 sigma (rms_min and rms_max, not the errorbars)."""
 
-    return parser.parse_args()
-
-
-def check_cli_arguments(args):
-    ierr = 0
-
-    # LATER CHECK IF ABSOLUTE PATHS ARE NEEDED
-    #if os.path.isabs(args.par_dirname) is False:
-    #    print('\n FATAL ERROR: .par file path is not absolute\n')
-    #    ierr = -1
-    #    return ierr
-
-    #if os.path.isabs(args.tim_dirname) is False:
-    #    print('\n FATAL ERROR: .tim file path is not absolute\n')
-    #    ierr = -1
-    #    return ierr
-
-    return ierr
-
-
-def calc_residuals(par_fname='', tim_fname=''):
-
-    #tim_fname = tim_dirname + '/' + self.pname + self.antenna + '.tim'
-    #par_fname = par_dirname + self.pname + '.par'
-
-    # file where timing residuals will be stored (use .tim file as reference)
-    res_fname = tim_fname.split('.ti')[0].split('/')[-1] + '.res'
-    log_fname = tim_fname.split('.ti')[0].split('/')[-1] + '.log'
-
-    line = '-residuals -us -f ' + par_fname + ' ' + tim_fname + ' > ' + log_fname
-
-    # call tempo2 and store the output residuals and the log (fitting information)
-    subprocess.call( 'tempo2 ' + line, shell=True )
-
-    # the default name for the residuals files is 'residuals.dat'
-    shutil.move('residuals.dat', res_fname)
-
-    # we read the rms of the fitting from the tempo2 log
-    rms = np.genfromtxt ( log_fname, comments="none", dtype=float, skip_header=18, max_rows=1, usecols=(10) )  
-
-    # calculate the rms+- delta_rms(1 sigma) --> (not the errorbars, rather rms_min and rms_max) 
-    timing = T2.tempopulsar(parfile = par_fname, timfile = tim_fname)
-    residuals = timing.residuals()
-    n_obs = timing.nobs 
-    #n_obs = len(residuals) 
     prob_84 = scipy.stats.chi2.ppf( 0.84, n_obs)
     prob_16 = scipy.stats.chi2.ppf( 0.16, n_obs)    
     rms_min = round( math.sqrt( (n_obs / prob_84 )) * rms, 4) 
     rms_max = round( math.sqrt( (n_obs / prob_16 )) * rms, 4)
-    print(rms, rms_min, rms_max)
+    return rms_min, rms_max
 
 
-def make_plot(par_fname='', tim_fname=''):
+def make_plot(par_fname='', tim_fname='', output_dir=''):
     """Plot residuals."""
 
-    import matplotlib.pyplot as P
+    # file where timing residuals will be stored (use .tim file as reference)
+    res_fname = output_dir + '/' + tim_fname.split('.tim')[0].split('/')[-1] + '.res'
 
-    timing = T2.tempopulsar(parfile = par_fname, timfile = tim_fname)
+    print('Calculating residuals')
 
+    timing = T2.tempopulsar(parfile=par_fname, timfile=tim_fname)
     res, t, errs = timing.residuals(), timing.toas(), timing.toaerrs
+
+    print('Saving residuals to ' + res_fname)
+
+    residuals_array = np.vstack((t, residuals, errs)).T
+    np.savetxt(res_fname, residuals_array)    
 
     print("Plotting {0} points.".format(timing.nobs))
 
-    meanres = math.sqrt(np.mean(res**2)) / 1e-6
-    
+    meanres = math.sqrt(np.mean(res**2)) * 1e6
+    rms = timing.rms()*1e6    # convert to us
+    rms_min, rms_max = calc_residuals_errorbars(n_obs=timing.nobs, rms=rms)
+
     i = np.argsort(t)
-    P.errorbar(t[i],res[i]/1e-6,yerr=errs[i],fmt='x')
+    P.errorbar(t[i], res[i]*1e6, yerr=errs[i], fmt='x')
         
     #P.legend(unique,numpoints=1,bbox_to_anchor=(1.1,1.1))
     P.xlabel('MJD'); P.ylabel('res [us]')
@@ -105,10 +57,40 @@ def make_plot(par_fname='', tim_fname=''):
 
     # Save
     pname = tim_fname.split('.tim')[0].split('/')[-1]
-    plot_output = pname + '_tempo.png'
+    plot_output = output_dir + '/' + pname + '_tempo.png'
     print('Saving plot to ' + plot_output)
     P.savefig(plot_output, bbox_inches='tight')
-    #LP.show()
+
+
+#=========================================================================
+# BELOW IS JUST FOR RUNNING AS INDEPENDENT PROGRAM   
+
+def set_argparse():
+    # add arguments
+    parser = argparse.ArgumentParser(prog='puma_timing.py',
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+            description='Calculate residuals using tempo2 and plot them')
+    parser.add_argument('--par_fname', default='', type=str,
+            help='absolute path to directory containing .par file')
+    parser.add_argument('--tim_fname', default='', type=str,
+            help='absolute path to .tim file')
+    parser.add_argument('--output_dir', default=os.environ['PWD'], type=str,
+            help='absolute path where the plot will be saved')
+
+    return parser.parse_args()
+
+
+def check_cli_arguments(args):
+    ierr = 0
+    if os.path.isabs(args.par_dirname) is False:
+        print('\n FATAL ERROR: .par file path is not absolute\n')
+        ierr = -1
+        return ierr
+    if os.path.isabs(args.tim_dirname) is False:
+        print('\n FATAL ERROR: .tim file path is not absolute\n')
+        ierr = -1
+        return ierr
+    return ierr
 
 
 if __name__ == '__main__':
@@ -119,17 +101,8 @@ if __name__ == '__main__':
     # check arguments
     ierr = check_cli_arguments(args)
     if ierr != 0: sys.exit(1)
-
-    # file where timing residuals will be stored (use .tim file as reference)
-    res_fname = args.tim_fname.split('.tim')[0].split('/')[-1] + '.res'
-    log_fname = args.tim_fname.split('.tim')[0].split('/')[-1] + '.log'
     
-    print('Calculating residuals')
-    calc_residuals(par_fname=args.par_fname, tim_fname=args.tim_fname)
-
-    print('Making residuals plot')
-    make_plot(par_fname=args.par_fname, tim_fname=args.tim_fname)
-
+    make_plot(par_fname=args.par_fname, tim_fname=args.tim_fname, output_dir=args.output_dir)
 
     #tim_fname = '/home/jovyan/work/shared/PuGli-S/tims/J0437-4715_A1.tim'  
     #par_fname = '/opt/pulsar/puma/config/timing//J0437-4715.par'
