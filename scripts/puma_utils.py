3c19fef935e46699e9bea19b176691835c1bcb13
egutierrezposse <puma@iar.unlp.edu.ar>
2020-06-12 17:58:39 +0000

    Fixed error in creating df_new for the first time for a PSR

--- scripts/puma_utils.py
+++ scripts/puma_utils.py
@@ -129,7 +129,8 @@ def write_pugliS_info_jason(path2pugliese,obs):
        df = pd.DataFrame()
 
    if len(df) == 0:
-       df = df.append(obs.__dict__, ignore_index=True)
+       print('The DB for this PSR was empty')
+       df_new = df.append(obs.__dict__, ignore_index=True)
 
    else:
        # We check whether and observation already exists and eliminate it
