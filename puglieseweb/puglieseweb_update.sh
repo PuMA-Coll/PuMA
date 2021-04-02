eec908d965426eb2f5a4fcee904c8c199b9000d7
Federico Garcia <fgarcia1012002@gmail.com>
2021-04-02 21:56:47 +0200

    Added RFI plots from GGANCIO to last_obs in PuglieseWeb.

--- puglieseweb/puglieseweb_update.sh
+++ puglieseweb/puglieseweb_update.sh
@@ -2,10 +2,18 @@ export WEBPATH="/home/observacion/scratchdisk/PuGli-S/puglieseweb/"
 export DBPATH="/home/observacion/scratchdisk/PuGli-S/"
 
 # Generate thumbnails
-echo 
+echo
 echo Generating thumbnails ...
 cd $DBPATH/last_obs/
 ~/bin/thumbnailer.sh
+
+# Get RFI plots from GANCIO
+echo Getting RFI plots
+cd $DBPATH/rfi/plots/
+wget -d http://tux.iar.unlp.edu.ar/~ggancio/pub/rfi/plots/Power_wide.png
+wget -d http://tux.iar.unlp.edu.ar/~ggancio/pub/rfi/plots/Spectrum_limited.png
+wget -d http://tux.iar.unlp.edu.ar/~ggancio/pub/rfi/plots/Spectrum_wide.png
+cd $DBPATH
 
 #Update the full website
 cd $WEBPATH
@@ -23,6 +31,9 @@ rsync -av --exclude '*txt' --exclude '*md' --exclude '*py' \
 rsync -av --exclude '*pfd' --exclude '*polycos' --exclude '*tim' --exclude '*json' \
           --exclude '*res' $DBPATH/J* lacar:public_html/private/
 
+#Update RFI plots
+scp $DBPATH/rfi/plots/* lacar:public_html/private/rfi/plots/
+
 #Update last_obs.html by_psr.html
 scp $DBPATH/*html lacar:public_html/private/
 
@@ -37,4 +48,3 @@ scp $DBPATH/mjd.txt lacar:public_html/private/
 echo
 echo 'Everything is up-to-date now.'
 echo
-
